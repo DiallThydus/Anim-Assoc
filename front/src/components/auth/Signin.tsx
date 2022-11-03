b6610f56cf445b02f324eae7d4fa5064ff567860
sonny-ipssi <s.lallier@ecole-ipssi.net>
2022-11-03 15:08:14 +0100

    création du front, ajout et configuration des dépendances

---
+++ front/src/components/auth/Signin.tsx
@@ -0,0 +1,8 @@
+export default function Signin() {
+    return (
+        <div className="flex flex-col space-y-4 text-center">
+            <h1 className="text-3xl">Ma super page d'inscription</h1>
+            <p>Ceci est la page d'inscription</p>
+        </div>
+    );
+}
---
+++ front/src/components/navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Button, Navbar } from "react-daisyui";
+import { useNavigate } from "react-router-dom";
+
+export default function NavbarComponent() {
+    const navigate = useNavigate();
+    return (
+        <>
+            <div className="flex w-full component-preview p-4 items-center justify-center gap-2 font-sans">
+                <Navbar className='flex justify-between'>
+                    <Button className="text-xl normal-case" color="ghost" onClick={() => navigate('/')}>
+                        Anim' Assoc
+                    </Button>
+                    <div className="flex space-x-4">
+                        <Button className="text-xl normal-case" color="primary" onClick={() => navigate('/signin')}>
+                            S'inscrire
+                        </Button>
+                        <Button className="text-xl normal-case" color="ghost" onClick={() => navigate('/login')}>
+                            Se connecter
+                        </Button>
+                    </div>
+                </Navbar>
+            </div>
+        </>
+    );
+}
