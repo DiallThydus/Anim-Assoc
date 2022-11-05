c7b4192bdee80b007dec11637067becc96a1512f
Youns <youns@MBP-de-Youns.com>
2022-11-05 03:15:09 +0100

    chore: Adoption/Anim

--- front/src/components/card/Adoption.tsx
+++ front/src/components/card/Adoption.tsx
@@ -3,12 +3,12 @@ import Maldives3 from "../../assets/maldives3.jpeg";
 import Anim from './Anim';
 
 const Adoption = () => {
-	return (
-		<div className='max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-			<Anim bg={Maldives3} text='Jamaica' />
-			<p>hello</p>
-		</div>
-	)
+  return (
+    <div className='max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <Anim bg={Maldives3} text='Jamaica' />
+      <p>hello</p>
+    </div>
+  )
 }
 
 export default Adoption;
