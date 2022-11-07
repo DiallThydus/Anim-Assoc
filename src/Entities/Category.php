10c51be1ed0c04974eeb63e5483e3fc5357bc4b6
Nicolas DESFORGES <froxik77430poutlook.fr>
2022-11-07 01:11:44 +0100

    interface controller peut être pas terminer

--- src/Entities/Category.php
+++ src/Entities/Category.php
@@ -2,7 +2,68 @@
 
 namespace App\Entities;
 
-class Category
+use Carbon\Carbon;
+use jsonSerialize;
+
+class Category implements JsonSerializable
 {
+    /**
+     * Category id
+     * PRIMARY KEY UNSIGNED NOT NULLABLE
+     *
+     * @var integer
+     */
+    private int $id;
+
+    /**
+     * Category name.
+     *
+     * @var string
+     */
+    private string $name;
+
+
+    //id
+    /**
+     * Get PRIMARY KEY UNSIGNED NOT NULLABLE
+     *
+     * @return  integer
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    //name
+    /**
+     * Get category name.
+     *
+     * @return  string
+     */ 
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set category name.
+     *
+     * @param  string  $name  Category name.
+     *
+     * @return  self
+     */ 
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+    
+    //Json
+    public function jsonSerialize(): array
+    {
+        return ['name' => $this->getName()];
+    }
+
 
 }
